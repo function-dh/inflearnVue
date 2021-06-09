41860d57ab9759d511098b737f4c705d975bb8c2
김동현 <kimdh@brandi.co.kr>
2021-06-09 15:50:46 +0900

    [작업] 기본 세팅 + TodoHeader 컴포넌트 작업

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoInput />
+    <TodoList />
+    <TodoFooter />
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import TodoFooter from './components/TodoFooter'
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
+}
+</script>
+
+<style>
+  body{
+    text-align: center;
+    background-color: #f6f6f6;
+  }
+  input{
+    width: 200px;
+    border-style: groove;
+  }
+  button{
+    border-style: groove;
+  }
+  body, input, textarea{
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: 500;
+  }
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0,0,0,003);
+  }
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<input type="text" v-model="newTodoItem" name="" id="">
+		<button @click="addTodo">add</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			newTodoItem: ''
+		}
+	},
+	methods: {
+		addTodo(){
+			console.log(this.newTodoItem);
+			this.newTodoItem = ''
+		},
+	},
+}
+</script>
+
+<style>
+
+</style>
