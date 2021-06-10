81b6f0f48133aac98edd44c703e7e97edaaf3bc1
김동현 <kimdh@brandi.co.kr>
2021-06-10 13:27:58 +0900

    [작업] input값 객체로 변경, TodoList 체크 리스트 추가

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
 	<div>
-		<ul v-for="(list, index) in todoItems" :key="index">
-			<li>{{ list }}</li>
+		<ul>
+			<li v-for="(todo, index) in todoItems" :key="index" class="shadow" >
+				<i :class="{checkBtnCompleted : todo.completed}" class="checkBtn fas fa-check" @click="toggleComplete(todo, index)"></i>
+				<span :class="{textCompleted : todo.completed}">{{ todo.item }}</span>
+				<span class="removeBtn" @click="removeTodo(todo.item, index)">
+					<i class="fas fa-trash-alt"></i>
+				</span>
+			</li>
 		</ul>
 	</div>
 </template>
@@ -13,18 +19,67 @@ export default {
 			todoItems : []
 		}
 	},
-	created() {
-		if(localStorage.length > 0 ){
-			for(let i = 0; i < localStorage.length; i++){
-				if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-					this.todoItems.push(localStorage.key(i))
+	methods: {
+		// localStorage에서 메시지 전달
+		requestMessage(){
+			if(localStorage.length > 0 ){
+				for(let i = 0; i < localStorage.length; i++){
+					// local webpack 정보 들어가지 않게
+					if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+						this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+					}
 				}
 			}
-		}
+		},
+		removeTodo(todo, index){
+			this.todoItems.splice(index, 1)
+			localStorage.removeItem(todo)
+		},
+		toggleComplete(todo){
+			todo.completed = !todo.completed
+
+			// 로컬 스토리지 갱신 (update가 없어.. 삭제 후 재등록)
+			localStorage.removeItem(todo.item)
+			localStorage.setItem(todo.item, JSON.stringify(todo))
+		},
+	},
+	created() {
+		this.requestMessage()
 	},
 }
 </script>
 
-<style>
-
+<style scoped>
+	ul {
+		list-style-type: none;
+		padding-left: 0px;
+		margin-top: 0;
+		text-align: left;
+	}
+	li {
+		display: flex;
+		min-height: 50px;
+		height: 50px;
+		line-height: 50px;
+		margin: 0.5rem 0;
+		padding: 0 0.9rem;
+		background: white;
+		border-radius: 5px;
+	}
+	.checkBtn {
+		line-height: 45px;
+		color: #62acde;
+		margin-right: 5px;
+	}
+	.checkBtnCompleted {
+		color: #b3adad;
+	}
+	.textCompleted {
+		text-decoration: line-through;
+		color: #b3adad;
+	}
+	.removeBtn {
+		margin-left: auto;
+		color: #de4343;
+	}
 </style>
