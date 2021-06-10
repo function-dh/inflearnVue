eff5e1b411eb4f76d1e6c5b67ae9cb5cd78512a0
김동현 <kimdh@brandi.co.kr>
2021-06-10 09:18:59 +0900

    [작업] input 스타일 추가, TodoList v-for 처리

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
-	<div>
-		<input type="text" v-model="newTodoItem" name="" id="">
-		<button @click="addTodo">add</button>
+	<div class="inputBox shadow">
+		<input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+		<button @click="addTodo" class="addContainer">
+			<i class="addBtn fas fa-plus" aria-hidden="true"></i>
+		</button>
 	</div>
 </template>
 
@@ -14,13 +16,43 @@ export default {
 	},
 	methods: {
 		addTodo(){
-			console.log(this.newTodoItem);
-			this.newTodoItem = ''
+			localStorage.setItem(this.newTodoItem, this.newTodoItem)
+			this.clearInput()
 		},
+		clearInput(){
+			this.newTodoItem = ''
+		}
 	},
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  height: 50px;
+  border-radius: 5px;
+  line-height: 50px;
+  background: #fff;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  display: block;
+  width: 3rem;
+	padding: 0;
+	border: 0;
+  border-radius: 0 5px 5px 0;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+}
+.addBtn {
+	height: 50px;
+  line-height: 50px;
+  vertical-align: middle;
+  color: #fff;
+}
 </style>
