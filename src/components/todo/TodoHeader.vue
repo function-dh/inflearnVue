a0f2ef840c897756c6a464ba78ed726cccfb7015
김동현 <ehakck7@gmail.com>
2021-06-22 13:35:11 +0900

    [작업] header 이모지 추가

--- src/components/todo/TodoHeader.vue
+++ src/components/todo/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="TodoHeader">
 		<div class="inner">
-			<h1 class="tit">메모장</h1>
+			<h1 class="tit"><i class="tit-emoji" aria-label="메모장 이모지">🗓</i>메모장</h1>
 		</div>
 	</div>
 </template>
@@ -13,6 +13,13 @@
 			text-align: center;
 			font-weight: 700;
 			color: $POINT;
+			.tit-emoji{
+				display: inline-block;
+				margin: 5px 4px 0 0;
+				line-height: 1;
+				font-style: normal;
+				vertical-align: top;
+			}
 		}
 	}
 </style>
